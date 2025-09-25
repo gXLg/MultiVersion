f09deb5c76b1aa26ff4f7af22af99324178b546a
gXLg <65429873+gXLg@users.noreply.github.com>
2025-09-25 22:08:32 +0200

    Update finalize.js

--- finalize.js
+++ finalize.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const { root, stub, package } = JSON.load(fs.readFileSync("./multi-version.json"));
+const { root, package } = JSON.load(fs.readFileSync("./multi-version.json"));
 
-const ref = "package " + package + ";\n\n" + fs.readFileSync("./MultiVersion/Reflection.java", "utf8");
+const ref = "package " + package + ";\n\n" + fs.readFileSync("./MultiVersion/Reflection.java", "utf-8");
 fs.writeFileSync(root + "/Reflection.java", ref);
