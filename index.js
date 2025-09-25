86079f3fdf4d16e3769a9c0b4b02d313cdb5e79c
gXLg <65429873+gXLg@users.noreply.github.com>
2025-09-25 21:34:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const fs = require("fs");
-const build = fs.readFileSync("./build.gradle", "utf-8");
-
 console.log("Welcome to MultiVersion!");
 console.log("Step 1: Download this module into the root of your project");
 console.log("Step 2: Configure your 'multi-version.json' file");
 console.log("Step 3: Add folowing to your 'build.gradle' file:");
-console.log(build);
+console.log("");
+console.log("apply from: './MultiVersion/multi-version.gradle'");
+console.log("");
 console.log("Step 4: Enjoy developing!");
