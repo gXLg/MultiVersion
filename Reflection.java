e51eccc9028e4b81093c85d101420154dd756327
gXLg <65429873+gXLg@users.noreply.github.com>
2025-10-10 16:22:51 +0200

    Update Reflection.java

--- Reflection.java
+++ Reflection.java
@@ -1,8 +1,5 @@
-import org.jetbrains.annotations.NotNull;
-
 @SuppressWarnings("unused")
 public class Reflection {
-    @NotNull
     public static Object wrap(String i) { return new Object(); }
     public static boolean version(String i) { return true; }
 }
