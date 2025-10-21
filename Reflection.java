9520cc62743709e7a664dd1fea498e55f6d91078
gXLg <65429873+gXLg@users.noreply.github.com>
2025-10-21 13:12:19 +0200

    Add wrapi method to Reflection class

--- Reflection.java
+++ Reflection.java
@@ -1,5 +1,6 @@
 @SuppressWarnings("unused")
 public class Reflection {
     public static Object wrap(String i, Object... ignored) { return new Object(); }
+    public static void wrapi(String i, Object... ignored) { }
     public static boolean version(String i) { return true; }
 }
