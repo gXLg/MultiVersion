482f71117fa28c8de9e2abcf0a41a735ceb56d6c
gXLg <65429873+gXLg@users.noreply.github.com>
2025-10-21 11:15:33 +0200

    Modify wrap method to accept varargs

--- Reflection.java
+++ Reflection.java
@@ -1,5 +1,5 @@
 @SuppressWarnings("unused")
 public class Reflection {
-    public static Object wrap(String i) { return new Object(); }
+    public static Object wrap(String i, Object... ignored) { return new Object(); }
     public static boolean version(String i) { return true; }
 }
