163e1e54fe59777dd200d1f1e8f33bda5314130a
gXLg <65429873+gXLg@users.noreply.github.com>
2025-10-21 16:58:31 +0200

    Add Nullable annotation to Reflection stub

--- Reflection.java
+++ Reflection.java
@@ -1,6 +1,11 @@
-@SuppressWarnings("unused")
+import org.jetbrains.annotations.Nullable;
+
+@SuppressWarnings({"unused", "DataFlowIssue"})
 public class Reflection {
+    @Nullable
     public static Object wrap(String i, Object... ignored) { return new Object(); }
+
     public static void wrapi(String i, Object... ignored) { }
+
     public static boolean version(String i) { return true; }
 }
