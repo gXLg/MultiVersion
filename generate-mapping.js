14c33213cec62f8506fa2fd220eb18f865ed261c
Dmitry Plesunov <dima.plesunov@gmx.de>
2026-01-18 23:53:25 +0100

    fix: handle inner classes better altogether

--- generate-mapping.js
+++ generate-mapping.js
@@ -42,8 +42,12 @@ function typeTree(type, additionalClasses) {
       }
     }
     const main = type.slice(0, start);
+    if (main.includes("/") || main.startsWith("!")) {
+      console.log("Wrapper classes shouldn't be generic!");
+      process.exit(1);
+    }
     const generics = gens.map(g => typeTree(g, additionalClasses));
-    return { "type": "generic", "main": type.replaceAll("$", "."), generics, "wrapped": generics.some(g => g.wrapped), "generic": true };
+    return { "type": "generic", "main": type, generics, "wrapped": generics.some(g => g.wrapped), "generic": true };
   }
   if (type.includes("/") || type.startsWith("!")) {
     if (type.startsWith("!")) {
@@ -59,7 +63,7 @@ function typeTree(type, additionalClasses) {
   if (type == "Object") {
     return { "type": "object", "main": "Object", "wrapped": false, "generic": false };
   }
-  return { "type": "java", "main": type.replaceAll("$", "."), "wrapped": false, "generic": false };
+  return { "type": "java", "main": type, "wrapped": false, "generic": false };
 }
 
 function buildTypeString(tree) {
@@ -70,7 +74,10 @@ function buildTypeString(tree) {
   if (type == "generic") {
     return main + "<" + generics.map(g => buildTypeString(g)).join(", ") + ">";
   }
-  return main;
+  if (type == "wrapper") {
+    return main;
+  }
+  return main.replaceAll("$", ".");
 }
 
 function buildClassGetter(tree) {
